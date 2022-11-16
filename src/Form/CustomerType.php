c7c5571b3a9576f55091567aecb54b32484bcff4
Marco Stoker <marcostokerdev@outlook.com>
2022-11-16 21:41:03 +0100

    added styling for customer views

--- src/Form/CustomerType.php
+++ src/Form/CustomerType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Customer;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +14,16 @@ class CustomerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('phone')
-            ->add('postalCode')
-            ->add('houseNumber')
+            ->add('name', TextType::class,
+                [ 'label' => 'Naam', 'attr' => [ 'class' => 'form-control' ], 'required' => true ])
+            ->add('email', EmailType::class,
+                [ 'label' => 'E-mailadres', 'attr' => [ 'class' => 'form-control' ], 'required' => true ])
+            ->add('phone', TextType::class,
+                [ 'label' => 'Telefoon', 'attr' => [ 'class' => 'form-control' ], 'required' => false ])
+            ->add('postalCode', TextType::class,
+                [ 'label' => 'Postcode', 'attr' => [ 'class' => 'form-control' ], 'required' => true ])
+            ->add('houseNumber', TextType::class,
+                [ 'label' => 'Huisnummer', 'attr' => [ 'class' => 'form-control' ], 'required' => true ])
         ;
     }
 
